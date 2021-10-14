76de40ccba330d41dbb60fd9d3a666b31fa9f790
Duckelekuuk <duckelekuuk@gmail.com>
2021-10-14 14:55:19 +0200

    Added command to plugin.yml and set the executor

--- src/main/java/com/duckelekuuk/player_vanish/PlayerVanish.java
+++ src/main/java/com/duckelekuuk/player_vanish/PlayerVanish.java
@@ -1,5 +1,6 @@
 package com.duckelekuuk.player_vanish;
 
+import com.duckelekuuk.player_vanish.commands.VanishCommand;
 import com.duckelekuuk.player_vanish.listeners.PlayerJoinListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,6 +10,6 @@ public class PlayerVanish extends JavaPlugin {
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
 
-        getCommand("vanish").setExecutor();
+        getCommand("vanish").setExecutor(new VanishCommand());
     }
 }
